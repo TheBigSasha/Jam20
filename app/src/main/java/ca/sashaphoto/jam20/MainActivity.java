6201e7cb299eadb81f8d6969c2e4c4cb6462d8f8
thebigsasha <sashathethird@gmail.com>
2020-11-13 21:23:39 -0500

    Add stuff for the sliders and shit.

--- app/src/main/java/ca/sashaphoto/jam20/MainActivity.java
+++ app/src/main/java/ca/sashaphoto/jam20/MainActivity.java
@@ -3,12 +3,21 @@ package ca.sashaphoto.jam20;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+    SeekBar howYouFeltSlider;
+    Button button_imbored;
+    EditText editTextFeedback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        howYouFeltSlider = findViewById(R.id.howYouFeltSlider);
+        button_imbored = findViewById(R.id.button_imbored);
+        editTextFeedback = findViewById(R.id.editTextFeedback);
     }
 }
