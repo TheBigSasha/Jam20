8736710b1b074732e1859277d9fd9664ca49527c
thebigsasha <sashathethird@gmail.com>
2020-11-14 12:53:33 -0500

    Add stuff for backend

--- app/src/main/java/ca/sashaphoto/jam20/JamBackend.java
+++ app/src/main/java/ca/sashaphoto/jam20/JamBackend.java
@@ -1,6 +1,7 @@
 package ca.sashaphoto.jam20;
 
 public class JamBackend {
+    private static final String[] hardCodes = new String[]{"Text a friend", "Make a pumpkin pie", "Learn what a REST API is"};
     public String getSuggestion() {
         return "WRITE THIS CODE";
         //TODO: Write this!
