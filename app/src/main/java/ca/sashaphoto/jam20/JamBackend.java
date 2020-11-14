b4c622e332e724cc7b65b035f6243ced96edd270
thebigsasha <sashathethird@gmail.com>
2020-11-14 12:52:11 -0500

    Add stuff for the sliders and shit.

---
+++ app/src/main/java/ca/sashaphoto/jam20/JamBackend.java
@@ -0,0 +1,8 @@
+package ca.sashaphoto.jam20;
+
+public class JamBackend {
+    public String getSuggestion() {
+        return "WRITE THIS CODE";
+        //TODO: Write this!
+    }
+}
--- app/src/main/java/ca/sashaphoto/jam20/MainActivity.java
+++ app/src/main/java/ca/sashaphoto/jam20/MainActivity.java
@@ -1,24 +1,31 @@
 package ca.sashaphoto.jam20;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
 
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.SeekBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     SeekBar howYouFeltSlider;
+    JamBackend backend;
     Button button_imbored;
     EditText editTextFeedback;
-    AppDatabase db = Room.databaseBuilder(getApplicationContext(),AppDatabase.class, "database-name").build();
+    //AppDatabase db = Room.databaseBuilder(getApplicationContext(),AppDatabase.class, "database-name").build();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        backend = new JamBackend();
         howYouFeltSlider = findViewById(R.id.howYouFeltSlider);
         button_imbored = findViewById(R.id.button_imbored);
         editTextFeedback = findViewById(R.id.editTextFeedback);
+        button_imbored.setOnClickListener(e -> getSuggestion());
+    }
+
+    private void getSuggestion() {
+        Toast.makeText(getBaseContext(),backend.getSuggestion(),Toast.LENGTH_SHORT).show();
     }
 }
