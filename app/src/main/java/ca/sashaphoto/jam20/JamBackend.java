7d00c6e689734d9a2402e516bf536f3b34f99186
thebigsasha <sashathethird@gmail.com>
2020-11-14 12:54:47 -0500

    Add hardcoded random suggestions!

--- app/src/main/java/ca/sashaphoto/jam20/JamBackend.java
+++ app/src/main/java/ca/sashaphoto/jam20/JamBackend.java
@@ -1,9 +1,12 @@
 package ca.sashaphoto.jam20;
 
+import java.util.Random;
+
 public class JamBackend {
     private static final String[] hardCodes = new String[]{"Text a friend", "Make a pumpkin pie", "Learn what a REST API is"};
+    private static final Random rand = new Random(System.nanoTime());
     public String getSuggestion() {
-        return "WRITE THIS CODE";
+        return hardCodes[rand.nextInt(hardCodes.length -1)];
         //TODO: Write this!
     }
 }
